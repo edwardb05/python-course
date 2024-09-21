08cd14a854c60bb16daaf6e1b24f26b1058e298f
edwardb05 <156453429+edwardb05@users.noreply.github.com>
2024-09-21 17:31:41 +0100

    finished flight data

--- flight-deals-start/data_manager.py
+++ flight-deals-start/data_manager.py
@@ -14,11 +14,16 @@ class DataManager:
         self._user = os.environ["SHEETY_USRERNAME"]
         self._password = os.environ["SHEETY_PASSWORD"]
         self._authorization = HTTPBasicAuth(self._user, self._password)
+        self.prices_endpoint = os.environ["SHEETY_PRICES_ENDPOINT"]
+        self.users_endpoint = os.environ["SHEETY_USERS_ENDPOINT"]
+        self._authorization = HTTPBasicAuth(self._user, self._password)
+# Destination and Customer fields data start out empty
+        self.customer_data = {}
         self.destination_data = {}
 
     def get_destination_data(self):
         # Use the Sheety API to GET all the data in that sheet and print it out.
-        response = requests.get(url=SHEETY_PRICES_ENDPOINT, auth= self._authorization)
+        response = requests.get(url=self.prices_endpoint, auth= self._authorization)
         data = response.json()
         response.raise_for_status()
         self.destination_data = data["prices"]
@@ -37,8 +42,18 @@ class DataManager:
                 }
             }
             response = requests.put(
-                url=f"{SHEETY_PRICES_ENDPOINT}/{city['id']}",
+                url=f"{self.prices_endpoint}/{city['id']}",
                 json=new_data,
                 auth = self._authorization
             )
             print(response.text)
+
+    def get_customer_emails(self):
+        response = requests.get(url=self.users_endpoint, auth=self._authorization)
+        data = response.json()
+        # See how Sheet data is formatted so that you use the correct column name!
+        # pprint(data)
+        # Name of spreadsheet 'tab' with the customer emails should be "users".
+        self.customer_data = data["users"]
+        print(self.customer_data)
+        return self.customer_data
--- flight-deals-start/main.py
+++ flight-deals-start/main.py
@@ -27,6 +27,11 @@ print(f"sheet_data:\n {sheet_data}")
 data_manager.destination_data = sheet_data
 data_manager.update_destination_codes()
 
+customer_data = data_manager.get_customer_emails()
+# Verify the name of your email column in your sheet. Yours may be different from mine
+customer_email_list = [row["whatIsYourEmailAddress?"] for row in customer_data]
+# print(f"Your email list includes {customer_email_list}")
+
 # ==================== Search for Flights and Send Notifications ====================
 tomorrow = datetime.now() + timedelta(days=1)
 six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
@@ -40,16 +45,34 @@ for destination in sheet_data:
         to_time=six_month_from_today
     )
     cheapest_flight = find_cheapest_flight(flights)
-    if cheapest_flight.price != "N/A" and cheapest_flight.price < destination["lowestPrice"]:
-        print(f"Lower price flight found to {destination['city']}!")
-        # notification_manager.send_sms(
-        #     message_body=f"Low price alert! Only £{cheapest_flight.price} to fly "
-        #                  f"from {cheapest_flight.origin_airport} to {cheapest_flight.destination_airport}, "
-        #                  f"on {cheapest_flight.out_date} until {cheapest_flight.return_date}."
-        # )
-        # SMS not working? Try whatsapp instead.
-        notification_manager.sendemail(
-            message_body=f"Low price alert! Only {cheapest_flight.price} to fly "
-                         f"from {cheapest_flight.origin_airport} to {cheapest_flight.destination_airport}, "
-                         f"on {cheapest_flight.out_date} until {cheapest_flight.return_date}."
-                         )
+    print(f"{destination['city']}: £{cheapest_flight.price}")
+    # Slowing down requests to avoid rate limit
+    time.sleep(2)
+
+    # ==================== Search for indirect flight if N/A ====================
+
+    if cheapest_flight.price == "N/A":
+        print(f"No direct flight to {destination['city']}. Looking for indirect flights...")
+        stopover_flights = flight_search.check_flights(
+            ORIGIN_CITY_IATA,
+            destination["iataCode"],
+            from_time=tomorrow,
+            to_time=six_month_from_today,
+            is_direct=False
+        )
+        cheapest_flight = find_cheapest_flight(stopover_flights)
+        print(f"Cheapest indirect flight price is: £{cheapest_flight.price}")
+
+    if cheapest_flight.stops == 0:
+        message = f"Low price alert! Only GBP {cheapest_flight.price} to fly direct "\
+        f"from {cheapest_flight.origin_airport} to {cheapest_flight.destination_airport}, "\
+        f"on {cheapest_flight.out_date} until {cheapest_flight.return_date}."
+    else:
+            message = f"Low price alert! Only GBP {cheapest_flight.price} to fly "\
+            f"from {cheapest_flight.origin_airport} to {cheapest_flight.destination_airport}, "\
+            f"with {cheapest_flight.stops} stop(s) "\
+            f"departing on {cheapest_flight.out_date} and returning on {cheapest_flight.return_date}."
+            print(f"Check your email. Lower price flight found to {destination['city']}!")
+        #
+        # Send emails to everyone on the list
+    notification_manager.send_emails(email_list=customer_email_list, email_body=message)
