b1a8c5a28bf215f5c2f56e9f6b4b0b0943706b15
edwardb05 <156453429+edwardb05@users.noreply.github.com>
2025-01-22 18:20:07 +0000

    scraping movies

--- web development/Starting Code - 100 movies to watch start/main.py
+++ web development/Starting Code - 100 movies to watch start/main.py
@@ -4,5 +4,13 @@ from bs4 import BeautifulSoup
 URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
 
 # Write your code below this line 👇
+text = requests.get(URL).text
 
+soup = BeautifulSoup(text, "html.parser")
+movies = soup.find_all('h3', class_ = 'title')
+movie_titles = [movie.getText() for movie in movies]
+
+with open("100movies.txt", "w") as file:
+    for movie in movie_titles:
+        file.write(f"{movie}\n")
 
